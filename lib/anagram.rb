cbd50e782450eb82ebcb495c270bb481a503f90a
Will Studabaker <will.studabaker@gmail.com>
2020-11-02 12:26:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialized(word)
+    @word = word
+  end
+  def match(array)
+    #compares each word in array to @word
+    match_array = []
+    array.each do |word|
+      match_array << word if word.split("").sort == @word.split("").sort
+    end
+    
+  end
+end
